78689fdbdbee8c8aeebb096c5e5d23f3f4ac6832
Asumu Takikawa <asumu@ccs.neu.edu>
2011-07-20 13:57:52 -0400

    Add info file

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang setup/infotab
+
+(define name "Sweet")
+
+(define blurb 
+  '("A port of the sweet reader that provides an alternative "
+    "syntax for s-expression languages."))
+
+(define categories
+  '(devtools metaprogramming))
