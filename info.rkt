c019eab01e0cb90bef22d6bdb4a67e7e56106899
Asumu Takikawa <asumu@ccs.neu.edu>
2011-07-20 14:22:01 -0400

    More info.

--- info.rkt
+++ info.rkt
@@ -6,5 +6,14 @@
   '("A port of the sweet reader that provides an alternative "
     "syntax for s-expression languages."))
 
+(define release-notes
+  '("1 0: Initial port to Racket"))
+
 (define categories
   '(devtools metaprogramming))
+
+(define scribblings
+  '(("sweet.scrbl")))
+
+(define repositories
+  '("4.x"))
