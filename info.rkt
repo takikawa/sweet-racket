38aa0ca2239b8a46b1f2e8b35c106de3c41da6e7
Asumu Takikawa <asumu@ccs.neu.edu>
2013-08-20 20:57:11 -0400

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,3 +1,6 @@
 #lang setup/infotab
 
-(define deps '())
+(define collection 'multi)
+(define deps '("base" "scribble-lib" "racket-doc" "lazy"
+               "scribble-doc" "rackunit-lib" "typed-racket-lib"))
+
