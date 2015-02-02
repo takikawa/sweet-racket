5f411f75ccf8d89d4ba1736f8bac78fe0d02dabc
Asumu Takikawa <asumu@ccs.neu.edu>
2015-02-01 23:32:07 -0500

    Enable sweet-exp reader in DrRacket interactions
    Use the runtime configuration mechanism of #lang to
    enable the sweet reader when a module written in #lang sweet-exp
    is run.

--- sweet-exp/main.rkt
+++ sweet-exp/main.rkt
@@ -2,30 +2,71 @@
 
 ;; Sweet expressions
 
-(module reader syntax/module-reader
-  #:language read
-  #:read sweet-read
-  #:read-syntax sweet-read-syntax
+(module link-reader racket/base
+
+  ;; This module handles the linking of units to construct
+  ;; a sweet expression reader.
 
   (require racket/unit
            "read-sig.rkt"
            "sweet.rkt")
 
-  (define-unit underlying-read@
-    (import)
-    (export (rename read^ [-read read]
-                          [-read-syntax read-syntax]))
+  (provide sweet-link)
 
-    (define -read read)
-    (define -read-syntax read-syntax))
+  ;; Procedure Procedure -> Procedure Procedure
+  ;; Constructs sweet readers from the given underlying readers
+  (define (sweet-link read read-syntax)
 
-  (define-values/invoke-unit
-    (compound-unit
+    (define-unit underlying-read@
       (import)
-      (export S)
+      (export (rename read^ [-read read]
+                            [-read-syntax read-syntax]))
 
-      (link [((R : read^)) underlying-read@]
-            [((S : read^)) sweet@ R]))
+      (define -read read)
+      (define -read-syntax read-syntax))
 
-    (import)
-    (export (prefix sweet- read^))))
+    (define-values/invoke-unit
+      (compound-unit
+        (import)
+        (export S)
+
+        (link [((R : read^)) underlying-read@]
+              [((S : read^)) sweet@ R]))
+
+      (import)
+      (export (prefix sweet- read^)))
+
+    (values sweet-read sweet-read-syntax)))
+
+(module language-info racket/base
+  (provide get-language-info)
+  (define (get-language-info data)
+    (lambda (key default)
+      (case key
+        [(configure-runtime)
+         '(#((submod sweet-exp runtime-config) configure #f))]
+        [else default]))))
+
+(module runtime-config racket/base
+
+  ;; This module provides runtime configuration, e.g., for
+  ;; using sweet-exp based modules in DrRacket
+
+  (require (submod ".." link-reader))
+
+  (provide configure)
+
+  (define (configure data)
+    (define-values (_ sweet-read-syntax)
+      (sweet-link read read-syntax))
+    (current-read-interaction sweet-read-syntax)))
+
+(module reader syntax/module-reader
+  #:language read
+  #:read sweet-read
+  #:read-syntax sweet-read-syntax
+  #:language-info #((submod sweet-exp language-info) get-language-info #f)
+
+  (require (submod ".." link-reader))
+  (define-values (sweet-read sweet-read-syntax)
+    (sweet-link read read-syntax)))
